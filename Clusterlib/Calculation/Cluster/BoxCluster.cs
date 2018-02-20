6dc0bb5de272d03dbc2ac4b36126c1ed55ba5c46
TrueDoctor <d-kobert@web.de>
2018-02-20 18:47:42 +0100

    test

--- Clusterlib/Calculation/Cluster/BoxCluster.cs
+++ Clusterlib/Calculation/Cluster/BoxCluster.cs
@@ -4,8 +4,7 @@
     using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Linq;
-
-    using ClusterSim.ClusterLib.Utility;
+    
 
     public class BoxCluster : ThreadCluster
     {
